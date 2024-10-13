ec62bc8908899fff7b390ffd649d2659f5d34b71
jun <an0yjun@naver.com>
2024-10-13 20:54:23 +0900

    라우터 적용

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -1,6 +1,17 @@
-import App from '@layouts/App';
-import { render } from 'react-dom';
 import React from 'react';
+import App from '@layouts/App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-console.log('clientisrun');
-render(<App />, document.querySelector('#app'));
+const container = document.getElementById('app');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+} else {
+  console.error('앱을 렌더링할 수 없습니다. #app 요소를 찾을 수 없습니다.');
+}
--- lecture/layouts/App/index.tsx
+++ lecture/layouts/App/index.tsx
@@ -1,7 +1,17 @@
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>App1111</div>;
+  console.log('hello');
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/next" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
