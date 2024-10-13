a2673ab5a9a7e254000aed3fc0ca430c45a1abc8
jun <an0yjun@naver.com>
2024-10-13 20:21:12 +0900

    초기 환경 설정

---
+++ lecture/client.tsx
@@ -0,0 +1,6 @@
+import App from '@layouts/App';
+import { render } from 'react-dom';
+import React from 'react';
+
+console.log('clientisrun');
+render(<App />, document.querySelector('#app'));
---
+++ lecture/layouts/App/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>App1111</div>;
+};
+
+export default App;
