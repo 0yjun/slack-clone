310ac36544472746665f0c64a70de31e85e96fd5
jun <an0yjun@naver.com>
2024-10-14 06:10:21 +0900

    코드 스플리팅 적용

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "@pages/Login"; // 애플리케이션의 메인 컴포넌트
-import SignUp from "@pages/SignUp"; // 애플리케이션의 메인 컴포넌트
+import loadable from "@loadable/component";
+const Login = loadable(() => import("@pages/Login"));
+const SignUp = loadable(() => import("@pages/SignUp"));
 
 const App = () => {
   return (
