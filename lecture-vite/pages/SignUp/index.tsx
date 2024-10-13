6a7f9234aaeba712d29e49a46c1e376d2a590e73
jun <an0yjun@naver.com>
2024-10-14 06:45:30 +0900

    회원가입 페이지 작성중

--- lecture-vite/pages/SignUp/index.tsx
+++ lecture-vite/pages/SignUp/index.tsx
@@ -1,5 +1,56 @@
-const index = () => {
-  return <div>회원가입</div>;
+import useInput from "@hooks/useInput";
+import { Input, Label, Header } from "./Styles";
+import fetcher from "@utils/fetcher";
+import { ChangeEvent, useCallback, useState } from "react";
+import useSWR from "swr";
+
+const SignUp = () => {
+  const { data, error, reavalidate } = useSWR("/api/users", fetcher);
+
+  const [email, onChangeEmail] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
+  const [password, , setPassword] = useInput("");
+  const [passwordCheck, , setPasswordCheck] = useInput("");
+  const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState("");
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+
+  const onChangePassword = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck]
+  );
+  return (
+    <div id="container">
+      <Header>slack-react</Header>
+      <Label id="email-label">
+        <span>이메일 주소</span>
+        <div>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={onChangeEmail}
+          />
+        </div>
+      </Label>
+      <Label id="nickname-label">
+        <span>닉네임</span>
+        <div>
+          <Input
+            type="nickname"
+            id="nickname"
+            name="nickname"
+            value={nickname}
+            onChange={onChangeNickname}
+          />
+        </div>
+      </Label>
+    </div>
+  );
 };
 
-export default index;
+export default SignUp;
