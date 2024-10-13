ed88c25de4528544d212efb4985b2ca56d5a3287
jun <an0yjun@naver.com>
2024-10-14 00:20:57 +0900

    react 18 라우팅 변경

--- front/client.tsx
+++ front/client.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "@layouts/App"; // 애플리케이션의 메인 컴포넌트
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("app");
+if (container == null) {
+  console.log("react init error in client.tsx");
+} else {
+  const root = createRoot(container); // createRoot(container!) if you use TypeScript
+  root.render(<App />);
+}
