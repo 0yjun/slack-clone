0ffe25f8fba3c3ef74e3dbabe8e7bbd1ca9b5a86
jun <an0yjun@naver.com>
2024-10-20 23:52:39 +0900

    폼 유효성검사 추가

---
+++ lecture-next/src/app/api/signup/route.ts
@@ -0,0 +1,9 @@
+// import { NextResponse } from "next/server";
+
+// export async function POST(request: Request) {
+//   try {
+//     const body = await request.json();
+//     const { name, email, password } = body;
+//     const response = await axios.
+//   } catch {}
+// }
--- lecture-next/src/app/signup/page.tsx
+++ lecture-next/src/app/signup/page.tsx
@@ -11,126 +11,121 @@ import {
 } from "@app/signup/styles";
 import { ChangeEvent, useCallback, useState } from "react";
 import useCRUD from "@/hooks/useCRUD";
+import { AxiosError } from "axios";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
+// type schemaType = {
+//   email: string;
+//   nickname: string;
+//   password: string;
+// };
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("유효한 이메일을 입력해 주세요")
+    .min(1, "이메일 입력해주세요"),
+  nickname: z.string().min(1, "닉네임을 입력해주세요."),
+  password: z.string().min(6, "비밀번호는 최소 6자 이상이어야 합니다."),
+  passwordCheck: z.string().min(6, "비밀번호 확인을 입력해주세요."),
+});
 
 const SignUp = () => {
-  const [email, onChangeEmail] = useInput("");
-  const [nickname, onChangeNickname] = useInput("");
-  const [password, , setPassword] = useInput("");
-  const [passwordCheck, , setPasswordCheck] = useInput("");
-  const [mismatchError, setMismatchError] = useState(false);
-  const [signUpError, setSignUpError] = useState(false);
-  const [signUpSuccess, setSignUpSuccess] = useState(false);
-  const { response, isError, error, performRequest } = useCRUD(
-    "/api/auth/signup",
-    "POST",
-    {
-      data: { email, nickname, password },
-    }
-  );
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "", // 초기값 설정
+      nickname: "", // 초기값 설정
+      password: "", // 초기값 설정
+      passwordCheck: "", // 초기값 설정
+    },
+  });
 
-  const onChangePassword = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setPassword(e.target.value);
-      setMismatchError(e.target.value !== passwordCheck);
-    },
-    [passwordCheck, setPassword]
-  );
-  const onChangePasswordCheck = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setPasswordCheck(e.target.value);
-      setMismatchError(e.target.value !== password);
-    },
-    [passwordCheck, setPasswordCheck]
-  );
+  const onSubmit = async (data) => {
+    const signupRequest = {
+      nickname: data.nickname,
+      email: data.email,
+      password: data.password,
+    };
+    try {
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupRequest),
+      });
+      if (!response.ok) {
+        alert("오류");
+      }
 
-  const onSubmit = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      console.log("onsubmit");
-      if (!nickname || !nickname.trim()) {
-        return;
-      }
-      if (mismatchError) {
-        setSignUpError(true);
-        setSignUpSuccess(false);
-        return;
-      }
-      performRequest();
-      if (isError) {
-        const errorMessage = error?.response
-          ? error.response
-          : "오류가 발생했습니다.";
-        alert(errorMessage);
-        setSignUpSuccess(false);
-        setSignUpError(true);
-      } else {
-        alert("성공");
-        console.log(response, isError, error);
-        setSignUpSuccess(true);
-        setSignUpError(false);
-      }
-    },
-    [email, nickname, password, mismatchError]
-  );
+      const responseData = await response.json();
+    } catch (error) {}
+  };
 
   return (
     <div id="container">
       <Header>slack-react</Header>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input
-              type="email"
-              id="email"
+            <Controller
               name="email"
-              value={email}
-              onChange={onChangeEmail}
+              control={control}
+              render={({ field }) => (
+                <Input type="email" id="email" {...field} />
+              )}
             />
+            {/* {errors.email && <Error>{errors.email}</Error>} */}
+            {errors.email && <Error>이메일 오류</Error>}
           </div>
         </Label>
         <Label id="nickname-label">
           <span>닉네임</span>
           <div>
-            <Input
-              type="nickname"
-              id="nickname"
+            <Controller
               name="nickname"
-              value={nickname}
-              onChange={onChangeNickname}
+              control={control}
+              render={({ field }) => (
+                <Input type="text" id="nickname" {...field} />
+              )}
             />
           </div>
+          {errors.email && <Error>닉네임 오류</Error>}
         </Label>
         <Label id="password-label">
           <span>비밀번호</span>
           <div>
-            <Input
-              type="password"
-              id="password"
+            <Controller
               name="password"
-              value={password}
-              onChange={onChangePassword}
+              control={control}
+              render={({ field }) => (
+                <Input type="password" id="password" {...field} />
+              )}
             />
           </div>
+          {(errors.password || errors.passwordCheck) && (
+            <Error>비밀번호 오류</Error>
+          )}
         </Label>
         <Label id="password-check-label">
           <span>비밀번호 확인</span>
           <div>
-            <Input
-              type="password"
-              id="password-check"
-              name="password-check"
-              value={passwordCheck}
-              onChange={onChangePasswordCheck}
+            <Controller
+              name="passwordCheck"
+              control={control}
+              render={({ field }) => (
+                <Input type="password" id="password-check" {...field} />
+              )}
             />
           </div>
-          {/* 확인 메세지 */}
-          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {signUpError && <Error>이미 가입된 이메일입니다.</Error>}
-          {signUpSuccess && (
-            <Success>회원가입되었습니다! 로그인해주세요.</Success>
-          )}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
